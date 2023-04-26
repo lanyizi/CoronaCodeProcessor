f91d94bbc30d769554dab73036a313323488788f
lanyizi <lanyi@ra3.moe>
2023-04-26 22:34:00 +0200

    Add project files.

---
+++ CoronaCodeProcessor/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+var excludedFolders = new[]
+{
+    "aptui",
+    "aptui_bak",
+    "aptui_bak2",
+    "aptui原版修改",
+};
+
+var logger = LogManager.GetLogger("Main");
+var workingDirectory = Environment.CurrentDirectory;
+logger.Debug("Working Directory: {workingDirectory}", workingDirectory);
+
